1fd02935488da8ee310aab299f40360bd1317805
门欣 <menxin@qq.com>
2020-02-12 15:56:46 +0800

    更改一些菜单的样式问题

--- src/Bundling/AdminLTEThemeGlobalScriptContributor.cs
+++ src/Bundling/AdminLTEThemeGlobalScriptContributor.cs
@@ -6,13 +6,13 @@ namespace MicroFeel.Abp.Theme.AdminLTE.Bundling
     {
         public override void ConfigureBundle(BundleConfigurationContext context)
         {
-            context.Files.Add("/themes/adminlte/jquery.min.js");
-            context.Files.Add("/themes/adminlte/bootstrap.bundle.min.js");
+            //context.Files.Add("/themes/adminlte/jquery.min.js");
+            //context.Files.Add("/themes/adminlte/bootstrap.bundle.min.js");
             context.Files.Add("/themes/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js");
             context.Files.Add("/themes/adminlte/dist/js/adminlte.js");
             context.Files.Add("/themes/adminlte/dist/js/demo.js");
-            context.Files.Add("/themes/adminlte/plugins/chart.js/Chart.js");
-            context.Files.Add("/themes/adminlte/dist/js/pages/dashboard2.js");
+            //context.Files.Add("/themes/adminlte/plugins/chart.js/Chart.js");
+            //context.Files.Add("/themes/adminlte/dist/js/pages/dashboard2.js");
         }
     }
 }
