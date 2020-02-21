66e44091342fe1b4a36ec280a143242e43760f43
高洁 <1798287688@qq.com>
2020-02-21 11:23:41 +0800

    添加捆绑压缩文件

--- src/Bundling/AdminLTEThemeGlobalScriptContributor.cs
+++ src/Bundling/AdminLTEThemeGlobalScriptContributor.cs
@@ -6,13 +6,9 @@ namespace MicroFeel.Abp.Theme.AdminLTE.Bundling
     {
         public override void ConfigureBundle(BundleConfigurationContext context)
         {
-            //context.Files.Add("/themes/adminlte/jquery.min.js");
-            //context.Files.Add("/themes/adminlte/bootstrap.bundle.min.js");
-            context.Files.Add("/themes/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js");
-            context.Files.Add("/themes/adminlte/dist/js/adminlte.js");
-            context.Files.Add("/themes/adminlte/dist/js/demo.js");
-            //context.Files.Add("/themes/adminlte/plugins/chart.js/Chart.js");
-            //context.Files.Add("/themes/adminlte/dist/js/pages/dashboard2.js");
+            context.Files.Add("/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js");
+            context.Files.Add("/themes/adminlte/js/adminlte.js");
+            context.Files.Add("/themes/adminlte/js/layout.js");
         }
     }
 }
--- src/Bundling/AdminLTEThemeGlobalStyleContributor.cs
+++ src/Bundling/AdminLTEThemeGlobalStyleContributor.cs
@@ -6,9 +6,10 @@ namespace MicroFeel.Abp.Theme.AdminLTE.Bundling
     {
         public override void ConfigureBundle(BundleConfigurationContext context)
         {
-            context.Files.Add("/themes/adminlte/plugins/fontawesome-free/css/all.min.css");
-            context.Files.Add("/themes/adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css");
-            context.Files.Add("/themes/adminlte/dist/css/adminlte.min.css");
+            context.Files.Add("/libs/Ionicons/css/ionicons.min.css");
+            context.Files.Add("/plugins/overlayScrollbars/css/OverlayScrollbars.min.css");
+            context.Files.Add("/themes/adminlte/css/adminlte.min.css");
+            context.Files.Add("/themes/adminlte/css/layout.css");
             context.Files.Add("https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700");
         }
     }
