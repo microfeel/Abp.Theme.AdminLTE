6a131489248d7d68b7c35f02aa2dd1f013534b53
门欣 <menxin@qq.com>
2020-03-01 17:39:47 +0800

    修正brandlogo 连接

---
+++ src/Localization/AdminLTEResource.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Localization;
+
+namespace MicroFeel.Abp.Theme.AdminLTE.Localization
+{
+    [LocalizationResourceName("AdminLTE")]
+    public class AdminLTEResource
+    {
+    }
+}
